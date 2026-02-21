913b0e34044a8b85cf6b7d77590d23327e587118
BigElderK <BigElderK@divineland.org>
2026-02-21 12:51:04 +0800

    feat(cmake): implement ArieoPackage function for package management and update build environment includes

---
+++ cmake/package/arieo_package.cmake
@@ -0,0 +1,34 @@
+cmake_minimum_required(VERSION 3.31)
+
+# Main dispatcher function
+function(ArieoPackage package)
+    set(oneValueArgs
+        CATEGORY
+    )
+
+    set(multiValueArgs 
+        COMPONENTS
+    )
+
+    cmake_parse_arguments(
+        ARGUMENT
+        ""
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN}
+    )
+
+    # log debug info about the project type
+    message(STATUS "Configuring package ${package_name} of category ${ARGUMENT_CATEGORY} with configure preset ${ARGUMENT_BUILD_CONFIGURE_PRESET}")
+    message(STATUS "Configuring package ${package_name} with components: ${ARGUMENT_COMPONENTS}")
+
+    # Set global variables for package name and category to be used in install function
+    # set(ARIEO_PACKAGE_NAME "${package}" CACHE INTERNAL "Name for Arieo package")
+    set(ARIEO_PACKAGE_NAME "${package}" CACHE INTERNAL "Name for Arieo package")
+    set(ARIEO_PACKAGE_CATEGORY "${ARGUMENT_CATEGORY}" CACHE INTERNAL "Category for Arieo packages")
+    add_custom_target(
+        ${package}
+        DEPENDS 
+            ${ARGUMENT_COMPONENTS}
+    )
+endfunction()
